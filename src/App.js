6fd2820e7339ab5bebd4c7bc70459cbd9b727d9e
luvi-kh <nalsengom@daum.net>
2024-09-29 15:03:12 +0900

    최종소스버전

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+import TodoList from './components/TodoList';
+import { useEffect } from 'react';
+import { useUserStore } from './store/useUserStore';
 
 function App() {
+
+  const { fetch, user } = useUserStore();
+  console.log('user', user);
+  useEffect(() => {
+    fetch(1);
+  }, [fetch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className='App-header'>
+        <Counter />
+        <TodoList />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {user.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {user.phone}
+        </p>
       </header>
     </div>
   );
